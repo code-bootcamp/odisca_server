6e938142e9c405b50e2c810c54ad6635d735f593
EunpyeongJeon <77541054a@gmail.com>
2023-05-13 12:24:37 +0900

    [FEATURE]userEntity작성

---
+++ src/apis/users/entities/user.entity.ts
@@ -0,0 +1,34 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+@ObjectType()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => String)
+  id: string;
+
+  @Column()
+  @Field(() => String)
+  name: string;
+
+  @Column()
+  @Field(() => String)
+  email: string;
+
+  @Column()
+  @Field(() => String)
+  password: string;
+
+  @Column()
+  @Field(() => String)
+  phone: string;
+
+  @Column()
+  @Field(() => Int)
+  point: number;
+
+  @Column()
+  @Field(() => String)
+  image: string;
+}
