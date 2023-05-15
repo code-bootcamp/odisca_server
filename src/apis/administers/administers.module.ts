793ed12e8b791043cc0588f01955d4aa2809436e
EunpyeongJeon <77541054a@gmail.com>
2023-05-15 10:15:42 +0900

    [FEATURE]AdministerAPI

---
+++ src/apis/administers/administers.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AdministersResolver } from './administers.resolver';
+import { AdministersService } from './administers.service';
+import { Administer } from './entities/administer.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Administer])],
+  providers: [AdministersResolver, AdministersService],
+  exports: [AdministersService],
+})
+export class AdministersModule {}
---
+++ src/apis/administers/interfaces/administers-service.interface.ts
@@ -0,0 +1,13 @@
+import { CreateAdministerInput } from '../dto/create-administer.input';
+
+export interface IAdministersServiceFindOne {
+  email: string;
+}
+
+export interface IAdministersServiceCreate {
+  createAdministerInput: CreateAdministerInput;
+}
+
+export interface IAdministersServiceFindOneById {
+  adminId: string;
+}
