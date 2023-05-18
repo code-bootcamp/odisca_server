5360526b0b46934f9737df7e26ae0052f0f19522
EunpyeongJeon <77541054a@gmail.com>
2023-05-18 09:57:42 +0900

    [FEATURE]StudyCafe API 수정

--- src/apis/studyCafes/studyCafes.service.ts
+++ src/apis/studyCafes/studyCafes.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { ImagesService } from '../images/images.service';
 import { StudyCafe } from './entities/studyCafe.entity';
 import {
   IStudyCafesServiceCreate,
@@ -15,17 +16,21 @@ export class StudyCafesService {
   constructor(
     @InjectRepository(StudyCafe)
     private readonly studyCafeRepository: Repository<StudyCafe>,
+    private readonly imageService: ImagesService,
   ) {}
 
   // 스터디 카페 등록
-  createStudyCafe({
+  async createStudyCafe({
     createStudyCafeInput,
     adminId,
   }: IStudyCafesServiceCreate): Promise<StudyCafe> {
-    return this.studyCafeRepository.save({
-      ...createStudyCafeInput,
+    const { image, ...StudyCafe } = createStudyCafeInput;
+    const result = await this.studyCafeRepository.save({
+      ...StudyCafe,
       administer: { id: adminId },
     });
+    await this.imageService.createCafeImage({ image, result });
+    return result;
   }
 
   // 스터디 카페 좌석배치도 등록
@@ -58,14 +63,21 @@ export class StudyCafesService {
   }
 
   // 스터디 카페 수정
-  update({
+  async updateStudyCafe({
     updateStudyCafeInput,
     adminId,
   }: IStudyCafesServiceUpdate): Promise<StudyCafe> {
-    return this.studyCafeRepository.save({
-      id: updateStudyCafeInput.studyCafeId,
-      ...updateStudyCafeInput,
+    const { image, ...StudyCafe } = updateStudyCafeInput;
+    const result = await this.studyCafeRepository.save({
+      ...StudyCafe,
       administer: { id: adminId },
     });
+    await this.imageService.update({ image, result });
+    return result;
+  }
+
+  // 좌석 수 등록
+  updateStudyCafeSeats({ studyCafeId, seatCount }) {
+    return this.studyCafeRepository.update({ id: studyCafeId }, { seatCount });
   }
 }
