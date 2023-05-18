c78d071c0f842a6b7742524fc8bf858774ad37de
EunpyeongJeon <77541054a@gmail.com>
2023-05-18 09:56:40 +0900

    [FEATURE]File업로드 API

---
+++ src/apis/files/files.resolver.ts
@@ -0,0 +1,20 @@
+import { UseGuards } from '@nestjs/common';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { FileUpload, GraphQLUpload } from 'graphql-upload';
+import { GqlAuthGuard } from '../auth/guards/gql-auth.guard';
+import { FilesService } from './files.service';
+
+@Resolver()
+export class FilesResolver {
+  constructor(
+    private readonly filesService: FilesService, //
+  ) {}
+
+  @UseGuards(GqlAuthGuard('access'))
+  @Mutation(() => [String])
+  uploadLoginImageFile(
+    @Args({ name: 'images', type: () => [GraphQLUpload] }) images: FileUpload[],
+  ): Promise<string[]> {
+    return this.filesService.upload({ images });
+  }
+}
