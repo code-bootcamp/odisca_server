5df99c5a2054689a5593e95e084df5d58da1418f
Middledown <middledown12@gmail.com>
2023-05-18 20:28:55 +0900

    [CHORE] .gitignore, app.module, main.ts 수정

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import { graphqlUploadExpress } from 'graphql-upload';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.use(graphqlUploadExpress());
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
