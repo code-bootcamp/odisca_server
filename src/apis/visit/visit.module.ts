535151dc8aa3ec812cc4fa2b6f113155e6ca9663
Middledown <middledown12@gmail.com>
2023-05-19 17:11:35 +0900

    [FEATURE] Visit fetchAll API 생성

--- src/apis/visit/visit.module.ts
+++ src/apis/visit/visit.module.ts
@@ -2,14 +2,18 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Visit } from './entities/visit.entity';
 import { VisitService } from './visit.service';
+import { VisitResolver } from './visit.resolver';
+import { User } from '../users/entities/user.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       Visit, //
+      User, //
     ]),
   ],
   providers: [
+    VisitResolver, //
     VisitService, //
   ],
 })
