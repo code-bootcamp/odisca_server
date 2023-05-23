431bde3ee0a51af03cbb2983046bad9fb14bd3c9
Middledown <middledown12@gmail.com>
2023-05-23 22:04:34 +0900

    [REFACTOR] Visit 조회 API 리팩토링

--- src/apis/visit/visit.module.ts
+++ src/apis/visit/visit.module.ts
@@ -3,27 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Visit } from './entities/visit.entity';
 import { VisitService } from './visit.service';
 import { VisitResolver } from './visit.resolver';
-import { User } from '../users/entities/user.entity';
-import { UsersService } from '../users/users.service';
-import { Image } from '../images/entities/image.entity';
-import { ImagesService } from '../images/images.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       Visit, //
-      User, //
-      Image,
     ]),
   ],
   providers: [
     VisitResolver, //
-    VisitService, //
-    UsersService, //
-    ImagesService,
-  ],
-  exports: [
-    // exports 설정
     VisitService, //
   ],
 })
