fe6d27904486f059031a83a9693546c6695a4c44
Middledown <middledown12@gmail.com>
2023-05-23 15:09:38 +0900

    [REFACTOR] Payment 코드 리팩토링

--- src/apis/payments/interfaces/payment-service.interface.ts
+++ src/apis/payments/interfaces/payment-service.interface.ts
@@ -1,5 +1,4 @@
 import { POINT_TRANSACTION_STATUS_ENUM } from 'src/apis/pointTransactions/entities/pointTransaction.entity';
-import { IAuthUser } from 'src/common/interfaces/context';
 
 export interface IPaymentsServiceCreate {
   payment_point: number;
