457c3d997c810e2c039d26b0ade0d8cc6890a21f
Middledown <middledown12@gmail.com>
2023-05-23 18:26:16 +0900

    [FIX] Nestjs 의존성 주입 충돌 수정

--- src/apis/users/users.module.ts
+++ src/apis/users/users.module.ts
@@ -11,9 +11,13 @@ import { VisitService } from '../visit/visit.service';
 import { User } from './entities/user.entity';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
+import { SeatsService } from '../seats/seats.service';
+import { Seat } from '../seats/entities/seat.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Visit, StudyCafe, Image, Review])],
+  imports: [
+    TypeOrmModule.forFeature([User, Visit, StudyCafe, Image, Review, Seat]),
+  ],
   providers: [
     UsersResolver,
     UsersService,
@@ -21,6 +25,7 @@ import { UsersService } from './users.service';
     StudyCafesService,
     ImagesService,
     ReviewsService,
+    SeatsService,
   ],
   exports: [UsersService],
 })
