b23f1f09a8af8d1bcc538722a622ca4d0244d7d1
Middledown <middledown12@gmail.com>
2023-05-24 18:47:58 +0900

    [FEATURE] Seat StudyCafe테이블 inUseSeat 업데이트 API

--- src/apis/seats/seats.module.ts
+++ src/apis/seats/seats.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Seat } from './entities/seat.entity';
 import { SeatsResolver } from './seats.resolver';
 import { SeatsService } from './seats.service';
+import { StudyCafe } from '../studyCafes/entities/studyCafe.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Seat])],
+  imports: [TypeOrmModule.forFeature([Seat, StudyCafe])],
   providers: [SeatsResolver, SeatsService],
 })
 export class SeatsModule {}
