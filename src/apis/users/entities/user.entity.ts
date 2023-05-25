f790a32b32ceef7136436de4ba7f6740ce7275e1
EunpyeongJeon <77541054a@gmail.com>
2023-05-25 14:49:52 +0900

    [FIX] StudyCafe 업데이트 API tnwjd

--- src/apis/users/entities/user.entity.ts
+++ src/apis/users/entities/user.entity.ts
@@ -35,8 +35,14 @@ export class User {
   @Field(() => Int)
   user_point?: number;
 
-  @Column({ default: 'defaultImageUrl' })
-  @Field(() => String, { defaultValue: 'defaultImageUrl' })
+  @Column({
+    default:
+      'https://storage.googleapis.com/wisc-storage/%E1%84%89%E1%85%B3%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%86%AB%E1%84%89%E1%85%A3%E1%86%BA%202023-05-25%20%E1%84%8B%E1%85%A9%E1%84%92%E1%85%AE%202.23.07.png',
+  })
+  @Field(() => String, {
+    defaultValue:
+      'https://storage.googleapis.com/wisc-storage/%E1%84%89%E1%85%B3%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%86%AB%E1%84%89%E1%85%A3%E1%86%BA%202023-05-25%20%E1%84%8B%E1%85%A9%E1%84%92%E1%85%AE%202.23.07.png',
+  })
   user_image?: string;
 
   @DeleteDateColumn()
